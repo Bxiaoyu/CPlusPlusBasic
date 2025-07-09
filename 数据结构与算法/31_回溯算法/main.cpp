b17176f437382829106fc26997e831fd8e971451
sky <star541086@163.com>
2025-07-09 17:32:17 +0800

    回溯算法

--- 数据结构与算法/31_回溯算法/main.cpp
+++ 数据结构与算法/31_回溯算法/main.cpp
@@ -39,3 +39,66 @@ int main()
 }
 #endif
 
+/*
+* 整数选择问题：给定一组数组，从里面挑出一组数，并让其整数之和与剩下的整数之和的差最小
+*/
+int arr[7] = { 12,6,7,11,16,3,8 };
+//const int length = sizeof(arr) / sizeof(arr[0]);
+const int length = 7;
+int x[7] = { 0 };  // 子集树辅助数组，记录节点走向左孩子还是右孩子。代表i节点被选择&未被选择
+int bestx[7] = { 0 };  // 记录最优解
+unsigned int minNum = 0xFFFFFFFF;  // 记录最小的差值
+int sum = 0;  // 记录所选子集数字的总和
+int r = 0;  // 记录未选择数字的和
+
+// 生成子集树
+void func(int i)
+{
+	if (i == length)
+	{
+		int result = abs(sum - r);
+		if (result < minNum)
+		{
+			minNum = result;
+			// 需要记录一下差值最小的子集
+			for (int j = 0; j < length; j++)
+			{
+				bestx[j] = x[j];
+			}
+		}
+	}
+	else
+	{
+		r -= arr[i];
+		sum += arr[i];
+		x[i] = 1;
+		func(i + 1);  // 选择i节点
+		sum -= arr[i];
+		r += arr[i];
+
+		x[i] = 0;
+		func(i + 1);  // 不选择i节点
+	}
+}
+
+int main()
+{
+	for (int v : arr)
+	{
+		r += v;
+	}
+
+	func(0);
+
+	for (int i = 0; i < length; i++)
+	{
+		if (bestx[i] == 1)
+		{
+			cout << arr[i] << " ";
+		}
+	}
+	cout << endl;
+	cout << "min:" << minNum << endl;
+
+	return 0;
+}
